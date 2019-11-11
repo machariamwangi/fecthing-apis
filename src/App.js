b6d172bfc8f846771f0a35f899adf9c30c07a221
Elimax Mwangi <elimax@localhost.localdomain>
2019-11-11 13:06:46 -0500

    finishup of the website from the apis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { Button } from 'react-bootstrap';
+import Recipe  from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App= ()=>{
+
+  const App_Id ="25da1cb9";
+  const App_key = 
+  "43021a2314e48f71149a41e7319774aa";
+   const [recipes, setRecipes] = useState([]);
+   const [search, setSearch] = useState("");
+   const [query, setQuery] = useState('chicken');
+
+        useEffect(() => {
+        getRecipes();
+        },[query]);
+
+const getRecipes= async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${App_Id}&app_key=${App_key}`);
+    const data = await response.json();
+   setRecipes(data.hits);
+   console.log(data.hits);
+
+    // alternative of loading date/fetching data from an api
+    // fetch(`https://api.edamam.com/search?q=chicken&app_id=${App_Id}&app_key=${App_key}`).then(response =>{
+    //   response.json();
+    // })
+}
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+    // console.log(search);
+  }
+  const  getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+  return(
+        <div className='App'>
+          <div className="container">
+        <form onSubmit={getSearch} className="search-form">
+          <input  className="search-bar" type="text" value={search} onChange={updateSearch} />
+          <Button  className="search-button" type="submit"variant="primary" active >Search</Button>
+         
+        </form>
+        <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe  key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories} 
+              image={recipe.recipe.image}
+              ingredients = {recipe.recipe.ingredients}
+           />
+        ))}
+        </div>
+        </div>
+        </div>
   );
 }
 
